b63b5a685cf451228156e8d1b6b732c3cddbf0c1
DanielAnufriev <anufrieff.daniel777@yandex.ru>
2024-04-05 16:40:49 +0300

    fix bugs

---
+++ SupportImageProcessorModule/src/main/java/DanielAnufrievGit/Mask.java
@@ -0,0 +1,11 @@
+package DanielAnufrievGit;
+
+public class Mask {
+    public final int[][] gx;
+    public final int[][] gy;
+
+    public Mask() {
+        this.gx = new int[][]{{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+        this.gy = new int[][]{{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+    }
+}
